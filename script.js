44f12f5a32f5584b82edb07b84d003d0512aae48
CzarNicholasKamo <matsielwakamo@gmail.com>
2026-01-21 19:53:33 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function getDirections() {
+  const entrance = document.getElementById("entrance").value;
+  const store = document.getElementById("store").value;
+
+  const directions = {
+    "JD Sports": "Located near Entrance 1, Upper Level.",
+    "La Parada": "On the Rooftop Piazza near Entrance 9.",
+    "Checkers LiquorShop": "Lower Level, Entrance 3.",
+    "Cosmic Comics": "Entrance 2, Lower Level.",
+    "Nespresso": "Upper Level, Entrance 8."
+    "Adidas": "Second level,  Entrance 1.",
+  };
+
+  const message = `From ${entrance}, head towards ${store}. ${directions[store]}`;
+  document.getElementById("output").innerText = message;
+}
